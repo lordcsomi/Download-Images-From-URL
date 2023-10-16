f05d8e974087c9c581f936dfe928a668bf12add9
csomi <csomos.adam.benedek@gmail.com>
2023-10-16 21:10:11 +0200

    v2

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import os
 import aiohttp
 import asyncio
+from PIL import Image
+from io import BytesIO
 
 # URL template
-base_url = ""
+base_url = "https://kisnaplo.karinthy.hu/app/interface.php?view=v_download_photo&target=PLWS{}.jpg&type=order&KSNPLSID=ndmqcnsi0uass2cqk1qt6vgv47&KSNPLSID=ndmqcnsi0uass2cqk1qt6vgv47"
+DOWNLOADE_PATH = "images"
 
 async def download_image(session, i):
     url = base_url.format(i)
@@ -10,33 +14,29 @@ async def download_image(session, i):
         if response.status == 200:
             image_content = await response.read()
             if len(image_content) >= 2000:
-                with open(f"image_{i}.jpg", "wb") as f:
-                    f.write(image_content)
-                print(f"Downloaded image_{i}.jpg")
+                # Validate the image using Pillow (PIL)
+                try:
+                    img = Image.open(BytesIO(image_content))
+                    img.verify()  # This will raise an error if the image is corrupt
+                    with open(os.path.join(DOWNLOADE_PATH, f"image_{i}.jpg"), "wb") as f:
+                        f.write(image_content)
+                    print(f"Downloaded image_{i}.jpg")
+                except Exception as e:
+                    print(f"Image_{i}.jpg is downloaded")
             else:
-                print(f"Image_{i}.jpg is too small")
+                print(f"Image_{i}.jpg is not available")
         else:
             print(f"Failed to download image_{i}.jpg")
 
 async def main():
+    # chechk if the download folder exists
+    if not os.path.exists(DOWNLOADE_PATH):
+        os.makedirs(DOWNLOADE_PATH)
+    
     async with aiohttp.ClientSession() as session:
-        # Load the list of checked images from a file (if it exists)
-        checked_images = set()
-        try:
-            with open("checked_images.txt", "r") as f:
-                checked_images = set(int(line.strip()) for line in f)
-        except FileNotFoundError:
-            pass
-
         # Loop through numbers 0-5000
-        for i in range(10001):
-            if i not in checked_images:
-                await download_image(session, i)
-                checked_images.add(i)
-
-                # Save the checked images to the file
-                with open("checked_images.txt", "a") as f:
-                    f.write(f"{i}\n")
+        for i in range(5001):
+            await download_image(session, i)
 
     print("Download complete.")
 
