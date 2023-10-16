9ee678fc3f40a599cc07c88b8903f0eb2059a015
csomi <csomos.adam.benedek@gmail.com>
2023-10-16 21:15:15 +0200

    trashold

--- main.py
+++ main.py
@@ -5,15 +5,16 @@ from PIL import Image
 from io import BytesIO
 
 # URL template
-base_url = "https://kisnaplo.karinthy.hu/app/interface.php?view=v_download_photo&target=PLWS{}.jpg&type=order&KSNPLSID=ndmqcnsi0uass2cqk1qt6vgv47&KSNPLSID=ndmqcnsi0uass2cqk1qt6vgv47"
+URL = "https://kisnaplo.karinthy.hu/app/interface.php?view=v_download_photo&target=PLWS{}.jpg&type=order&KSNPLSID=ndmqcnsi0uass2cqk1qt6vgv47&KSNPLSID=ndmqcnsi0uass2cqk1qt6vgv47"
 DOWNLOADE_PATH = "images"
+TRASHOLD = 2000 # 2KB
 
 async def download_image(session, i):
-    url = base_url.format(i)
+    url = URL.format(i)
     async with session.get(url) as response:
         if response.status == 200:
             image_content = await response.read()
-            if len(image_content) >= 2000:
+            if len(image_content) >= TRASHOLD:
                 # Validate the image using Pillow (PIL)
                 try:
                     img = Image.open(BytesIO(image_content))
